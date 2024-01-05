719a3e2da24e257eaefaae908e3dfb9d1ba9e498
AndroidHQ254 <d.murimi14@gmail.com>
2024-01-05 21:19:39 +0300

    P661N: Define bootloader & platform info
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,14 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+BOARD_VENDOR := ITEL
+TARGET_BOOTLOADER_BOARD_NAME := itel-P661N
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6833
+BOARD_HAS_MTK_HARDWARE := true
+
 # Inherit the proprietary files
 include vendor/itel/P661N/BoardConfigVendor.mk
